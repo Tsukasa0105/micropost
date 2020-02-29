e7e6f4924a34fec9dc46955c6f3340f7144ba199
tsukasa kuribayashi <tsukasa.kuribayashi@techacademy.jp>
2020-02-29 05:01:35 +0000

    change02291401

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
-  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
-  has_many :liked, through: :reverses_of_favorite, source: :user
+  # has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+  # has_many :user, through: :reverses_of_favorite
   
   validates :content, presence: true, length: { maximum: 255 }
   
