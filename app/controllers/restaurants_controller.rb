2d6a8eb6b7dc227ec33126e2c86f3a65bce0e407
tsukasa kuribayashi <tsukasa.kuribayashi@techacademy.jp>
2020-05-18 14:42:15 +0000

    add gurunabi API

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,18 @@
 class RestaurantsController < ApplicationController
   def new
+  end 
+  
+  def search 
+  url = https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=,{6535da406d2e7e47ee9ee620ca91f061}
+  if params[:search]
+    word=params[:search]
+    url << "&name=" << word
   end
+  url = URI.encode(url)
+  uri = URI.parse(url)
+  json = NET::HTTP.get(uri)
+  result = JSON.parse(json)
+    @rests = result["rest"] 
+end 
+  
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,19 @@
 class ToppagesController < ApplicationController
   def index
+
     if logged_in?
+      require "net/http"
+      url = 'https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=6535da406d2e7e47ee9ee620ca91f061'
+      if params[:search]
+        url = url + "&freeword=" + params[:search]
+        url = URI.encode(url) 
+        uri = URI.parse(url)
+        json = Net::HTTP.get(uri)
+        result = JSON.parse(json)
+        @rests = result["rest"]
+      else
+        @rests = []
+      end
       @micropost = current_user.microposts.build  # form_with 用
       @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
     end
